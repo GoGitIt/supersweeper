b46274100b9396f95534beb7e947219e1820c754
Jeff Valle <jeffreyvalle@protonmail.com>
2019-01-23 14:25:43 -0800

    Game disables clicking when game has ended

--- client/components/GameBoard.jsx
+++ client/components/GameBoard.jsx
@@ -18,7 +18,10 @@ class GameBoard extends React.Component {
     }
 
     handleClick(e) {
-        console.log(e);
+        //Identify when the game has ended by checking div id and then do nothing. Disables handleClick.
+        if (document.getElementById('gameover')) {
+            return;
+        }
 
         if (document.getElementById(e + ',mine').textContent !== 'M') {
             var coor = e.split(',');
@@ -33,73 +36,57 @@ class GameBoard extends React.Component {
 
             var mineCount = 0;
 
-
-            console.log('?', coor, left, topLeft, bottom)
             if (document.getElementById(top) && document.getElementById(top + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('top')
             }
             if (document.getElementById(topRight) && document.getElementById(topRight + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('tr')
             }
             if (document.getElementById(right) && document.getElementById(right + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('r')
             }
             if (document.getElementById(bottomRight) && document.getElementById(bottomRight + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('br')
             }
             if (document.getElementById(bottom) && document.getElementById(bottom + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('b')
             }
             if (document.getElementById(bottomLeft) && document.getElementById(bottomLeft + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('bl')
             }
             if (document.getElementById(left) && document.getElementById(left + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('l')
             }
             if (document.getElementById(topLeft) && document.getElementById(topLeft + ',mine').textContent === 'M') {
                 mineCount++;
-                console.log('tl')
             }
-
-            console.log(mineCount)
 
             if (mineCount === 0) {
                 this.handleZeros(coor, top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft);
             }
 
             if (e.split(',').length === 3) {
-                console.log(document.getElementById(e).textContent)
                 if (document.getElementById(e).textContent !== "M") {
                     document.getElementById(e).textContent = mineCount;
                 }
                 document.getElementById(e).style.visibility = "visible";
             } else {
-                console.log(document.getElementById(e + ',mine').textContent)
                 if (document.getElementById(e).textContent !== "M") {
                     document.getElementById(e + ',mine').textContent = mineCount;
                 }
                 document.getElementById(e + ',mine').style.visibility = "visible";
             }
-        } else {
-            // alert('GAME OVA');
+        } else if (document.getElementById(e + ',mine').textContent === 'M') {
             this.handleGameOver();
-        } 
+        }
     }
 
     handleGameOver () {
-        console.log('Game over called')
         for (let i = 0; i < this.state[this.state.level].length; i++) {
             for(let j = 0; j < this.state[this.state.level].length; j++) {
-                console.log('Game over coords:', [i, j].toString())
                 if (document.getElementById([i, j].toString() + ',mine').textContent === 'M') {
                     document.getElementById([i, j].toString() + ',mine').style.visibility = "visible";
+                    document.getElementById([i, j].toString() + ',mine').id = 'gameover';
                 }
             }
         }
@@ -107,41 +94,32 @@ class GameBoard extends React.Component {
 
     handleZeros (coord, top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft) {
         let zerosArr = this.state.zeros;
-        console.log('Accumulating found zeros:', this.state.zeros, 'what is ', top)
         if(this.state.zeros.includes(coord.join(',')) === false) {
             zerosArr.push(coord.join(','));
-            console.log('Not seen zero coord')
+
             if (document.getElementById(top) && document.getElementById(top + ',mine').textContent !== 'M') {
                 this.handleClick(top);
-                console.log('1')
             }
             if (document.getElementById(topRight) && document.getElementById(topRight + ',mine').textContent !== 'M') {
                 this.handleClick(topRight);
-                console.log('2')
             }
             if (document.getElementById(right) && document.getElementById(right + ',mine').textContent !== 'M') {
                 this.handleClick(right);
-                console.log('3')
             }
             if (document.getElementById(bottomRight) && document.getElementById(bottomRight + ',mine').textContent !== 'M') {
                 this.handleClick(bottomRight);
-                console.log('4')
             }
             if (document.getElementById(bottom) && document.getElementById(bottom + ',mine').textContent !== 'M') {
                 this.handleClick(bottom);
-                console.log('5')
             }
             if (document.getElementById(bottomLeft) && document.getElementById(bottomLeft + ',mine').textContent !== 'M') {
                 this.handleClick(bottomLeft);
-                console.log('6')
             }
             if (document.getElementById(left) && document.getElementById(left + ',mine').textContent !== 'M') {
                 this.handleClick(left);
-                console.log('7')
             }
             if (document.getElementById(topLeft) && document.getElementById(topLeft + ',mine').textContent !== 'M') {
                 this.handleClick(topLeft);
-                console.log('8')
             }
         }
     }
